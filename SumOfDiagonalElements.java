ce1d5fbe676ca5b2dc46fdc18ad050a9e6a10967
govindraj111 <37842596+govindraj111@users.noreply.github.com>
2018-04-02 09:10:43 +0530

    Update SumOfDiagonalElements.java

--- SumOfDiagonalElements.java
+++ SumOfDiagonalElements.java
@@ -2,9 +2,8 @@ class SumOfDiagonalElements {
      
     public static int difference(int arr[][], int n)
     {
-        
-        int d1 = 0, d2 = 0;
-      
+        int d1 = 0, d2 = 0;        
+         
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
@@ -30,9 +29,9 @@ class SumOfDiagonalElements {
           
         int arr[][] =
         {
-            {11, 2, 4},
-            {4 , 5, 6},
-            {10, 8, -12}
+            {16, 24, 5},
+            {1 , 1, 4},
+            {13, 7, 3}
         };
       
         System.out.print(difference(arr, n));
